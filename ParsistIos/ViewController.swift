713fec896e8ccea029f187f6b587c65d18354f21
Erdo <erdo@erdos-mac.local>
2019-01-21 05:19:26 -0800

    Initial Commit

---
+++ ParsistIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParsistIos
+//
+//  Created by Erdo on 21.01.2019.
+//  Copyright © 2019 Erdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
